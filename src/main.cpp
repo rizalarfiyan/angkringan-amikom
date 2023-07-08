bcc2d68711b00cbe020265da137be859094caab7
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-07-08 15:44:01 +0700

    fix: error in proccess order

--- src/main.cpp
+++ src/main.cpp
@@ -189,13 +189,13 @@ public:
                 cout << "| ORDER ID      : " << setw(17) << left << currentNode->order.orderId  << " |" << endl;
                 cout << "| Username     : " << setw(17) << left << currentNode->order.user.username << " |" << endl;
                 cout << "| Price    : $" << setw(16) << fixed << setprecision(2) << left << currentNode->order.totalPrice << " |" << endl;
-                cout << "| Quantity : " << setw(17) << left << order.quantity << " |" << endl;
+                cout << "| Quantity : " << setw(17) << left << currentNode->order.quantity << " |" << endl;
                 cout << "+-----------------------------+" << endl;
 
                 cout << "Order ID: " << currentNode->order.orderId << endl;
                 cout << "Username: " << currentNode->order.user.username << endl;
                 cout << "Quantity: " << currentNode->order.quantity << endl;
-                cout << "Price: $" << fixed << setprecision(2) << currentNode->o << endl;
+                cout << "Price: $" << fixed << setprecision(2) << currentNode->order.totalPrice << endl;
                 cout << endl;
                 currentNode = currentNode->next;
             }
